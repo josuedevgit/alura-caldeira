5baa71e4ccb1625eb6e7e8cfd603dcc982d5d9bb
Josué <josueluzsilva.01@gmail.com>
2023-06-29 16:22:24 -0300

    mais um tanto

---
+++ js-arrays/atualizando-lista.js
@@ -0,0 +1,13 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+nomes.splice(1, 2, "Rodrigo"); // limpar as duas posições e adiciona no lugar "Rodrigo"
+
+// nomes.push("Rodrigo")
+
+console.log(nomes);
+
+const animaisDoAquario = ['🐋', '🐙', '🐬', '🦈']
+
+animaisDoAquario.splice(1,0,'🐠')
+animaisDoAquario.splice(3,2,'🐟')
+
+console.log(animaisDoAquario)
